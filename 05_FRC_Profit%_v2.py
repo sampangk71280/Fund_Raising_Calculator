2a826d70536ca76968c105e13b31f686367c0cfe
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-05-19 11:40:38 +1200

    working profit $ or % function

--- 05_FRC_Profit%_v2.py
+++ 05_FRC_Profit%_v2.py
@@ -5,21 +5,26 @@
 # Get user's total
 # Multiply by percentage, or find the difference
 
+# function that asks
 def profit_check(question, number):
     valid = False
     while not valid:
+        # errpr message
         error = "Please enter an exact number or percentage you want to raise!"
 
         try:
             response = input(question).lower()
+            # ask user how much if they reply with dollar
             if response in dollar:
                 response = int(input("How much? "))
+            # ask user what percent if they reply with percentage
             elif response in percent:
                 response = int(input("What percentage? "))
 
-
+            # response has to be higher than a minimum number
             if number < response:
                 return response
+            # if not, print error message
             else:
                 print(error)
                 print()
@@ -30,7 +35,10 @@ def profit_check(question, number):
 
 
 # main routine
+
+# possible user response
 dollar = ["$", "dollar", "dollars"]
 percent = ["%","percentage", "percent"]
+#test run
 a = profit_check("Do you want to increase the profit by percentage (%) or dollars ($)?", 0)
 print(a)
